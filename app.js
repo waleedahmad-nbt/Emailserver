f380bd37d0a2fa3d25789f44119456ba818582e6
NBTJS-02 <awaisnbt@gmail.com>
2023-08-01 13:16:07 +0500

    Adding Email Services

--- app.js
+++ app.js
@@ -1,20 +1,55 @@
-const express = require("express")
-require("dotenv").config()
+const express = require("express");
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const cors = require("cors")
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(cors())
 
-app.use(express.json())
+// Create a Nodemailer transporter with your email service provider's settings
+const transporter = nodemailer.createTransport({
+  // Configure your email service provider settings here
+  service: "gmail",
+  auth: {
+    user: "awaisweb4@gmail.com",
+    pass: "foqesciiohulpnfa",
+  },
+});
 
-app.get("/" , (req , res)=>{
-    res.send("Server Runing Susscesfully")
-})
+app.post("/sendEmail", async (req, res) => {
+    
+  const { firstName, lastName, companyName, toEmail, phone, message } =
+    req.body;
 
-app.get("/Email" , (req , res)=>{
-    res.send("Email Send Susscesfully")
-})
+  try {
+    // Compose the email content
+    const emailContent = `
+        First Name: ${firstName}
+        Last Name: ${lastName}
+        Company Name: ${companyName}
+        Your work email: ${toEmail}
+        Phone: ${phone}
+        Message: ${message}
+      `;
 
-const port  = process.env.PORT || 4000
+    // Send the email
+    await transporter.sendMail({
+      from: "awaisweb4@gmail.com", // Replace with your email address
+      to: `${req.body.toEmail}, "awaisweb4@gmail.com`, // Replace with the recipient's email address
+      subject: "Newslatter Form Submission",
+      text: emailContent,
+    });
 
-app.listen(port , ()=>{
-    console.log("Server is Runing on " + port);
-})
+    res.json({ message: "Email sent successfully." });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ error: "Error sending email." });
+  }
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log("Server is Runing on " + port);
+});
